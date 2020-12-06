e0df46c25cc6e9f6265b0f2a08d53539fef414ea
yatharth1122 <55340379+yatharth1122@users.noreply.github.com>
2020-12-06 11:33:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def home():
 @app.route("/predict", methods = ["POST"])
 @cross_origin()
 def predict():
-    if request.method == "POST":
+   # if request.method == "POST":
 
         # Date_of_Journey
         date_dep = request.form["Dep_Time"]
